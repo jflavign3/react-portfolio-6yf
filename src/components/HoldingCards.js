b03cc9f1ff90d67809e9d0e69ffed9ee89a75e47
Jean-Francois Lavigne <jean-francois.lavigne@radio-canada.ca>
2023-07-17 16:35:32 -0400

    sdfg

---
+++ src/components/HoldingCards.js
@@ -0,0 +1,86 @@
+import { useState } from "react";
+import { GetStaleData } from "../DAL/GetStaleData.js";
+import { GetLiveData } from "../DAL/GetLiveData.js";
+import HoldingCard from "./HoldingCard/HoldingCard.js";
+import { ToastContainer, toast } from "react-toastify";
+
+let didInit = false;
+
+const HoldingCards = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [isMarketHours, setIsMarketHours] = useState(true);
+  const [stateHoldings, setStateHoldings] = useState([]);
+  const [activeCardId, setActiveCardId] = useState(null);
+
+  //var aa = GetLiveData();
+
+  //to do, put in TS
+  const deleteHolding = (id) => {
+    var holdings = stateHoldings.filter((x) => x.id !== id);
+    //debugger;
+    setStateHoldings(holdings);
+  };
+
+  const expandCard = (id) => {
+    setActiveCardId(id);
+  };
+
+  const RefreshData = async () => {
+    //debugger;
+
+    toast.info(
+      `Getting market data.`
+    );
+    setStateHoldings(await GetLiveData());
+    toast.success(
+      `Finished getting market data.`
+    );
+    setIsLoading(false);
+  };
+
+  if (!didInit) {
+    didInit = true;
+    console.log(`initializing. Set stale data.`);
+    // debugger;
+    setStateHoldings(GetStaleData()); //set inital data without prices, so that the card render immediately. Price will come after
+    //why cant do await?
+
+    if (isMarketHours) {
+      console.log(`In market hours, refrshing data on init`);
+      RefreshData();
+    } else {
+      setIsLoading(false);
+    }
+  }
+
+  if (isLoading) {
+    //console.log("still fetching stock data");
+  }
+  if (!isLoading) {
+    console.log("finished fetching stock data");
+    console.log(stateHoldings);
+  }
+
+  return (
+    <>
+      <section className="section" id="tours">
+        <div id="notification"></div>
+        <div className="section-center featured-center">
+          {stateHoldings.map((currentHolding) => {
+            return (
+              <HoldingCard
+                key={currentHolding.id}
+                isLoading={isLoading}
+                currentHolding={currentHolding}
+                deleteHolding={deleteHolding}
+                activeCardId={activeCardId}
+                expandCard={expandCard}
+              ></HoldingCard>
+            );
+          })}
+        </div>
+      </section>
+    </>
+  );
+};
+export default HoldingCards;
